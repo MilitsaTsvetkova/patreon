a565fcb96282100e91346240fa273cbf51f324a3
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-31 18:21:57 +0200

    onboarding pages

--- src/components/WelcomeOne.tsx
+++ src/components/WelcomeOne.tsx
@@ -1,5 +1,83 @@
+"use client";
+import { DollarSignIcon, InboxIcon, SparkleIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
+
+import { useRouter } from "next/navigation";
+import { api } from "../trpc/react";
+
 const WelcomeOne = () => {
-  return <div>one</div>;
+  const router = useRouter();
+  const updateProfile = api.profile.updateProfile.useMutation({
+    onSuccess: () => {
+      router.push("/welcome?step=2");
+    },
+  });
+  return (
+    <Card className="w-[400px]">
+      <CardHeader>
+        <CardTitle>Welcome to Patreon</CardTitle>
+        <CardDescription>
+          There are already more than 250,000 creators on Patreon and
+          there&apos;s about to be one more
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="border-t-2 pt-3">
+        <ul className="spae-y-3 my-4 text-sm">
+          <li className="flex items-center space-x-4">
+            <span className="w-6 text-gray-400">
+              <SparkleIcon />
+            </span>
+            <span>Start your own Patreon in just a few steps.</span>
+          </li>
+          <li className="flex items-center space-x-4">
+            <span className="w-6 text-gray-400">
+              <InboxIcon />
+            </span>
+            <span>Invite fans and connect with them for free.</span>
+          </li>
+          <li className="flex items-center space-x-4">
+            <span className="w-6 text-gray-400">
+              <DollarSignIcon />
+            </span>
+            <span>Easy to offer paid memberships.</span>
+          </li>
+        </ul>
+      </CardContent>
+      <CardFooter className="flex flex-col space-y-4">
+        <Button
+          className="w-full"
+          onClick={() => {
+            updateProfile.mutate({
+              type: "creator",
+            });
+          }}
+          disabled={updateProfile.isLoading}
+        >
+          Continue
+        </Button>
+        <Button
+          variant="ghost"
+          className="w-full"
+          disabled={updateProfile.isLoading}
+          onClick={() => {
+            updateProfile.mutate({
+              type: "fan",
+            });
+          }}
+        >
+          Not a creator? Join as a fan
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default WelcomeOne;
--- src/components/WelcomeTwo.tsx
+++ src/components/WelcomeTwo.tsx
@@ -1,5 +1,75 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { api } from "../trpc/react";
+import { useState } from "react";
+
 const WelcomeTwo = () => {
-  return <div>two</div>;
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+  const updateProfile = api.profile.updateProfile.useMutation();
+  return (
+    <Card className="w-[400px]">
+      <CardHeader>
+        <CardTitle>Let&apos;s name your Patreon</CardTitle>
+        <CardDescription>You can always change this later.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form action="" className="flex flex-col gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">Page Name</Label>
+            <Input
+              id="name"
+              placeholder="Your page name"
+              required
+              onChange={(e) => setName(e.target.value)}
+            ></Input>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              placeholder="Your  username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            ></Input>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col space-y-4">
+        <Button
+          className="w-full"
+          onClick={() => {
+            updateProfile.mutate({
+              name,
+              username,
+            });
+            router.push("/creator/profile");
+          }}
+        >
+          Next
+        </Button>
+        <Button
+          variant="ghost"
+          className="w-full"
+          onClick={() => router.back()}
+        >
+          Back
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default WelcomeTwo;
